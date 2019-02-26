2a83b03b9a0b9d4d3b557a7c11b7d06fa44ed2f6
isaacoo <2saacoo@gmail.com>
2019-02-27 01:56:52 +0900

    2.2.2 테스트의 효율적인 수행과 결과 관리

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -1,15 +1,19 @@
 package springbook.user.dao;
 
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
-
 import java.sql.SQLException;
 
+
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        // ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    @Test
+    public void addAndGet() throws SQLException{
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
@@ -24,10 +28,12 @@ public class UserDaoTest {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
 
-        System.out.println(user2.getPassword());
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+    }
 
-        System.out.println(user2.getId() + " 조회 성공");
+    public static void main(String[] args) {
+        JUnitCore.main("springbook.user.dao.UserDaoTest");
     }
 }
