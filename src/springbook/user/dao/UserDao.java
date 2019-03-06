241f0047021ddc0db33254a77f710edcf8d004ba
isaacoo <2saacoo@gmail.com>
2019-03-06 23:23:40 +0900

    3.4.2 JdbcContext의 특별한 DI

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -8,16 +8,16 @@ import java.sql.*;
 
 public class UserDao {
     private DataSource dataSource;
+    private JdbcContext jdbcContext;
 
     public void setDataSource(DataSource dataSource) {
+        this.jdbcContext = new JdbcContext();
+
+        this.jdbcContext.setDataSource(dataSource);
+
         this.dataSource = dataSource;
     }
 
-    private JdbcContext jdbcContext;
-
-    public void setJdbcContext(JdbcContext jdbcContext){
-        this.jdbcContext = jdbcContext;
-    }
 
     public void add(final User user) throws SQLException {
         this.jdbcContext.workWithStatementStrategy(
