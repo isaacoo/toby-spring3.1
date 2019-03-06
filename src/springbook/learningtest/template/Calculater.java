987ca0c2dcd1a7db6a8b8b803a66115b8b3844e6
isaacoo <2saacoo@gmail.com>
2019-03-07 01:36:35 +0900

    3.5.3 템플릿/콜백의 응용 - 템플릿/콜백의 재설계

--- src/springbook/learningtest/template/Calculater.java
+++ src/springbook/learningtest/template/Calculater.java
@@ -26,31 +26,48 @@ public class Calculater {
         }
     }
 
-    public Integer calcSum(String filepath) throws IOException {
-        BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
-            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                Integer sum = 0;
-                String line = null;
-                while ((line = br.readLine()) != null) {
-                    sum += Integer.valueOf(line);
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer res = initVal;
+            String line = null;
+            while ((line = br.readLine()) != null){
+                res = callback.doSomethingWithLine(line, res);
+            }
+            return res;
+        } catch (IOException e){
+            e.getMessage();
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
                 }
-                return sum;
+            }
+        }
+
+    }
+
+    public Integer calcSum(String filepath) throws IOException {
+        LineCallback sumCallback = new LineCallback() {
+            @Override
+            public Integer doSomethingWithLine(String line, Integer value) {
+                return value + Integer.valueOf(line);
             }
         };
-        return fileReadTemplate(filepath, sumCallback);
+        return lineReadTemplate(filepath, sumCallback, 0);
     }
 
     public Integer calcMultiply(String filepath) throws IOException {
-        BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
-            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                Integer multiply = 1;
-                String line = null;
-                while ((line = br.readLine()) != null){
-                    multiply *= Integer.valueOf(line);
-                }
-                return multiply;
+        LineCallback multiplyCallback = new LineCallback() {
+            @Override
+            public Integer doSomethingWithLine(String line, Integer value) {
+                return value * Integer.valueOf(line);
             }
         };
-        return fileReadTemplate(filepath, multiplyCallback);
+        return lineReadTemplate(filepath, multiplyCallback, 1);
     }
 }
