7fe00e32368040e69ac894c0e65f6162b6b67efa
isaacoo <2saacoo@gmail.com>
2019-03-12 00:21:06 +0900

    3.6.4 query()

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -95,6 +95,9 @@ public class UserDaoTest {
     public void getAll() throws SQLException{
         dao.deleteAll();
 
+        List<User> users0 = dao.getAll();
+        assertThat(users0.size(), is(0));
+
         dao.add(user1); // Id: gyumee
         List<User> users1 = dao.getAll();
         assertThat(users1.size(), is(1));
