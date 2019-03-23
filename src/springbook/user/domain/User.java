dd9a60db83e4db04b3c1054485cb85a95ed440cc
isaacoo <2saacoo@gmail.com>
2019-03-23 13:29:48 +0900

    5.1.1 필드 추가 - User 필드 추가

--- src/springbook/user/domain/User.java
+++ src/springbook/user/domain/User.java
@@ -5,15 +5,18 @@ public class User {
     String name;
     String password;
 
-    public User(String id, String name, String password){
+    Level level;
+    int login;
+    int recommend;
+
+    public User(String id, String name, String password) {
         this.id = id;
         this.name = name;
         this.password = password;
     }
 
-    public User(){
+    public User() {
     }
-
 
     public String getId() {
         return id;
@@ -37,6 +40,30 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public int getLogin() {
+        return login;
+    }
+
+    public void setLogin(int login) {
+        this.login = login;
+    }
+
+    public int getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(int recommend) {
+        this.recommend = recommend;
     }
 }
 
