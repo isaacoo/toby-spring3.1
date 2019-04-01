b4ddb0c2dfdb328150a59870d2566062086e9127
isaacoo <2saacoo@gmail.com>
2019-04-02 00:06:03 +0900

    5.1.3 UserService.upgradeLevels() - UserServiceTest 테스트 클래스

---
+++ src/springbook/user/service/UserService.java
@@ -0,0 +1,13 @@
+package springbook.user.service;
+
+import springbook.user.dao.UserDao;
+
+public class UserService {
+    UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
+}
