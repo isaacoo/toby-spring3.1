71e597f896a566cd2facb3adaadf8347f21ef9ee
isaacoo <2saacoo@gmail.com>
2019-04-24 23:54:11 +0900

    6.3.4 다이내믹 프록시를 위한 팩토리 빈 - 팩토리 빈의 설정 방법

---
+++ src/springbook/learningtest/spring/factorybean/FactoryBeanTest.java
@@ -0,0 +1,26 @@
+package springbook.learningtest.spring.factorybean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.learningtest.jdk.Message;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class FactoryBeanTest {
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    public void getMessageFromFactoryBean() {
+        Object message = context.getBean("message");
+        assertThat(message, instanceOf(Message.class));
+        assertThat(((Message)message).getText(), is("Factory Bean"));
+    }
+}
