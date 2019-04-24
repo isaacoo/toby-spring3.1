6b903bdcf856960eb561e1bbf2151c198a023775
isaacoo <2saacoo@gmail.com>
2019-04-25 01:58:18 +0900

    6.4.1 ProxyFactoryBean - 포인트컷: 부가기능 적용 대상 메소드 선정 방법

--- src/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
+++ src/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -4,6 +4,8 @@ import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import springbook.learningtest.jdk.Hello;
 import springbook.learningtest.jdk.HelloTarget;
 import springbook.learningtest.jdk.HelloUppercase;
@@ -53,5 +55,22 @@ public class DynamicProxyTest {
             String ret = (String) invocation.proceed();
             return ret.toUpperCase();
         }
+    }
+
+    @Test
+    public void pointcutAdvisor() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("sayH*");
+
+        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+
+        Hello proxiedHello = (Hello) pfBean.getObject();
+
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("Thank You Toby"));
     }
 }
